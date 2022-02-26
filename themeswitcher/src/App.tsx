eb235d0ddde9d315ba876e71191cd83a525cec06
marcoscrocci <marcoscrocci@gmail.com>
2022-02-25 22:44:31 -0300

    Styled Components - Theme Switcher - Implementação dos temas Light e Dark.

--- themeswitcher/src/App.tsx
+++ themeswitcher/src/App.tsx
@@ -1,10 +1,29 @@
-import React from "react";
-import logo from "./logo.svg";
+import React from 'react';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
+import usePeristedState from './utils/usePersistedState'
 
-function App() {
-    return <div className="App">
-        <div>Olá Mundo!</div>
-    </div>;
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';   
+
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+
+const App = () => {
+  const [theme, setTheme] = usePeristedState<DefaultTheme>('theme', light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <GlobalStyle />
+        <Header toggleTheme={toggleTheme} />
+        <div>Texto do aplicativo</div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
