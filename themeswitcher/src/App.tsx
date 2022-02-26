d3d2aabdd2e1cbde102d7757e1714be7e835ee0f
marcoscrocci <marcoscrocci@gmail.com>
2022-02-25 22:16:39 -0300

    Styled Components - Theme Switcher - Criação do projeto e instalação das bibliotecas necessárias

---
+++ themeswitcher/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+
+function App() {
+    return <div className="App">
+        <div>Olá Mundo!</div>
+    </div>;
+}
+
+export default App;
